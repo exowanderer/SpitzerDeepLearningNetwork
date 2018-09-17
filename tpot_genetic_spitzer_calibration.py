d8585496e4532624b8461b3e27f56493f6a1c3aa
Jonathan Fraine <exowanderer@users.noreply.github.com>
2018-09-17 15:43:13 +0300

    updated for DF usage instead of NP

--- tpot_genetic_spitzer_calibration.py
+++ tpot_genetic_spitzer_calibration.py
@@ -14,8 +14,10 @@ from sklearn.metrics          import r2_score
 
 from time import time
 
-features = pd.read_csv('pmap_raw_16features.csv')
-labels = pd.read_csv('pmap_raw_labels_and_errors.csv')
+
+n_skip = 100 # testing on smaller data set
+features = pd.read_csv('pmap_raw_16features.csv')[::n_skip]
+labels = pd.read_csv('pmap_raw_labels_and_errors.csv')[::n_skip]
 
 #Split training, testing, and validation data
 training_indices, validation_indices = train_test_split(idx, test_size=0.20)
@@ -24,13 +26,13 @@ training_indices, validation_indices = train_test_split(idx, test_size=0.20)
 tpot = TPOTRegressor(generations=10, verbosity=2, n_jobs=-1)
 
 start = time()
-tpot.fit(features[training_indices], labels[training_indices])
+tpot.fit(features.iloc[training_indices].values, labels.iloc[training_indices].values)
 print('Full TPOT regressor operation took {:.1f} minutes'.format((time() - start)/60))
 
 #Score the accuracy
 
 print('Best pipeline test accuracy: {:.3f}'.format(
-  tpot.score(features[validation_indices].values, labels[validation_indices])))
+  tpot.score(features.iloc[validation_indices].values, labels.iloc[validation_indices].values)))
 
 #Export the generated code
 tpot.export('spitzer_calibration_tpot_best_pipeline.py')
