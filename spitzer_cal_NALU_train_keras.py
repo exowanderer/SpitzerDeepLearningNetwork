4230b088125c732201718b0088db259478968696
Jonathan <jdfraine@gmail.com>
2018-09-19 10:44:05 +0300

    updated NALU train with faster inputs

--- spitzer_cal_NALU_train_keras.py
+++ spitzer_cal_NALU_train_keras.py
@@ -49,8 +49,6 @@ except:
 verbose = args['verbose']
 data_set_fname = args['data_set']
 
-print('BEGIN BIG COPY PASTE ')
-
 import pandas as pd
 import numpy as np
 
@@ -76,144 +74,9 @@ from glob                     import glob
 from time import time
 start0 = time()
 
-def setup_features(dataRaw, label='flux', notFeatures=[], pipeline=None, verbose=False, resample=False, returnAll=None):
-    
-    """Example function with types documented in the docstring.
 
-        For production level usage: All scaling and transformations must be done 
-            with respect to the calibration data distributions
-        
-        Args:
-            features  (nD-array): Array of input raw features.
-            labels    (1D-array): The second parameter.
-            pipeline       (int): The first parameter.
-            label_scaler   (str): The second parameter.
-            feature_scaler (str): The second parameter.
-        Returns:
-            features_transformed, labels_scaled
-
-        .. _PEP 484:
-            https://github.com/ExoWanderer/
-
-    """
-    # if label in notFeatures: notFeatures.remove(label)
-    
-    if isinstance(dataRaw,str):
-        dataRaw = pd.read_csv(filename)
-    elif isinstance(dataRaw, dict):
-        dataRaw = pd.DataFrame(dataRaw)
-    elif not isinstance(dataRaw, pd.DataFrame):
-        raise TypeError('The input must be a `pandas.DataFrame` or a `dict` with Equal Size Entries (to convert to df here)')
-    
-    # WHY IS THIS ALLOWED TO NOT HAVE PARENTHESES?
-    # assert isinstance(dataRaw, pd.DataFrame), 'The input must be a Pandas DataFrame or Dictionary with Equal Size Entries'
-    
-    inputData = dataRaw.copy()
-    
-    # PLDpixels = pd.DataFrame({key:dataRaw[key] for key in dataRaw.columns if 'pix' in key})
-    pixCols = [colname for colname in inputData.columns if 'pix' in colname.lower() or 'pld' in colname.lower()]
-    
-    PLDnorm             = np.sum(np.array(inputData[pixCols]),axis=1)
-    inputData[pixCols]  = (np.array(inputData[pixCols]).T / PLDnorm).T
-    
-    # # Overwrite the PLDpixels entries with the normalized version
-    # for key in dataRaw.columns:
-    #     if key in PLDpixels.columns:
-    #         inputData[key] = PLDpixels[key]
-    #
-    # Assign the labels
-    n_PLD   = len([key for key in dataRaw.keys() if 'err' not in colname.lower() and ('pix' in key.lower() or 'pld' in key.lower())])
-    
-    input_labels  = [colname for colname in dataRaw.columns if colname not in notFeatures and 'err' not in colname.lower()]
-    errors_labels = [colname for colname in dataRaw.columns if colname not in notFeatures and 'err'     in colname.lower()]
-    
-    # resampling_inputs = ['flux', 'xpos', 'ypos', 'xfwhm', 'yfwhm', 'bg_flux', 'bmjd', 'np'] + ['pix{}'.format(k) for k in range(1,10)]
-    # resampling_errors = ['fluxerr', 'xerr', 'yerr', 'xerr', 'yerr', 'sigma_bg_flux', 'bmjd_err', 'np_err'] + ['fluxerr']*n_PLD
-    
-    start = time()
-    
-    if resample:
-        print("Resampling ", end=" ")
-        inputData = pd.DataFrame({colname:np.random.normal(dataRaw[colname], dataRaw[colerr]) \
-                                    for colname, colerr in tqdm(zip(input_labels, errors_labels), total=len(input_labels))
-                                 })        
-    
-        print("took {} seconds".format(time() - start))
-    else:
-        inputData = pd.DataFrame({colname:dataRaw[colname] for colname in input_labels})
-    
-    labels  = dataRaw[label].values
-    
-    # explicitly remove the label
-    if label in inputData.columns: inputData.drop(label, axis=1, inplace=True)
-    
-    feature_columns = [colname for colname in inputData.columns if colname not in notFeatures]
-    print('\n\n','flux' in notFeatures, 'flux' in feature_columns, '\n\n')
-    features = inputData[feature_columns].values
-    
-    if verbose: print('Shape of Features Array is', features.shape)
-    
-    if verbose: start = time()
-    
-    # labels_scaled     = labels# label_scaler.fit_transform(labels[:,None]).ravel() if label_scaler   is not None else labels
-    features_trnsfrmd = pipeline.fit_transform(features) if pipeline is not None else features
-    
-    if verbose: print('took {} seconds'.format(time() - start))
-    
-    collection = features_trnsfrmd, labels
-    
-    if returnAll == True:
-        collection = features_trnsfrmd, labels, pipeline
-    
-    if returnAll == 'features':
-        collection = features_trnsfrmd
-    
-    if returnAll == 'with raw data':
-        collection.append(dataRaw)
-    
-    return collection
-
-# ## Load CSVs data
-tobe_flux_normalized = ['fluxerr', 'bg_flux', 'sigma_bg_flux', 'flux']
-spitzerCalNotFeatures = ['flux', 'fluxerr', 'dn_peak', 'xycov', 't_cernox', 'xerr', 'yerr', 'sigma_bg_flux']
-spitzerCalFilename = 'pmap_ch2_0p1s_x4_rmulti_s3_7.csv' if data_set_fname == '' else data_set_fname
-
-spitzerCalRawData = pd.read_csv(spitzerCalFilename)
-
-for key in tobe_flux_normalized:
-    spitzerCalRawData[key] = spitzerCalRawData[key] / np.median(spitzerCalRawData['flux'].values)
-
-spitzerCalRawData['bmjd_err'] = np.median(0.5*np.diff(spitzerCalRawData['bmjd']))
-spitzerCalRawData['np_err'] = np.sqrt(spitzerCalRawData['yerr'])
-
-for colname in spitzerCalRawData.columns:
-    if 'err' not in colname.lower() and ('pix' in colname.lower() or 'pld' in colname.lower()):
-        spitzerCalRawData[colname+'_err'] = spitzerCalRawData[colname] * spitzerCalRawData['fluxerr']
-
-start = time()
-print("Transforming Data ", end=" ")
-
-operations = []
-# header = 'GBR' if do_gbr else 'RFI' if do_rfi else 'STD'
-
-pipe = Pipeline(operations) if len(operations) else None
-
-not_features_now = []
-for feat_name in spitzerCalNotFeatures:
-    if feat_name in spitzerCalRawData.columns:
-        not_features_now.append(feat_name)
-
-features, labels, pipe_fitted = setup_features( dataRaw = spitzerCalRawData, 
-                                                pipeline = pipe, 
-                                                verbose = verbose,
-                                                notFeatures = not_features_now,
-                                                resample = False,
-                                                returnAll = True)
-
-print('END OF BIG COPY PASTE')
 
 print('BEGIN NEW HyperParameter Optimization.')
-
 from sklearn.metrics import r2_score
 
 
